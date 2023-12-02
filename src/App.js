8a6916eb1074fefe6cddb9405c54f04bf85fe662
dangdegeamu <dangdegeamu@gmail.com>
2023-12-02 22:51:46 +0700

    lol

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import './Todo.css';
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos'));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    if (input.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: input, completed: false }]);
+      setInput('');
+    }
+  };
+
+  const toggleTodo = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const deleteAllCompleted = () => {
+    const updatedTodos = todos.filter((todo) => !todo.completed);
+    setTodos(updatedTodos);
+  };
+
+  const filterTodos = () => {
+    switch (filter) {
+      case 'active':
+        return todos.filter((todo) => !todo.completed);
+      case 'completed':
+        return todos.filter((todo) => todo.completed);
+      default:
+        return todos;
+    }
+  };
+
+  return (
+    <div className="TodoApp">
+      <h1>#Todo</h1>
+      <div className="filter-buttons">
+        <button onClick={() => setFilter('all')} className={filter === 'all' ? 'active' : ''}>
+          All
+        </button>
+        <button onClick={() => setFilter('active')} className={filter === 'active' ? 'active' : ''}>
+          Active
+        </button>
+        <button onClick={() => setFilter('completed')} className={filter === 'completed' ? 'active' : ''}>
+          Completed
+        </button>
+      </div>
+      <form onSubmit={addTodo} className="add-todo-form">
+        <input
+          type="text"
+          placeholder="add details"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul className="todo-list">
+        {filterTodos().map((todo) => (
+          <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <span className="todo-text">{todo.completed ? 'Task done' : todo.text}</span>
+            {todo.completed && (
+              <button onClick={() => deleteTodo(todo.id)} className="delete-button">
+                🗑️
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+      {filter === 'completed' && (
+        <div>
+          <button onClick={deleteAllCompleted} className="delete-all-button">
+Delete All
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default TodoApp;
